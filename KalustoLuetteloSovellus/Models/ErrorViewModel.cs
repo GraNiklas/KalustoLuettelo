f34fa8646695840467e8b745f252abbef237165d
Niklas Granqvist <niklasgranqvist@windowslive.com>
2025-03-04 20:54:07 +0200

    Add project files.

---
+++ KalustoLuetteloSovellus/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KalustoLuetteloSovellus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
