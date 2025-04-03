68605061ae2b4a45279c9a99d18c3e75b7b778f7
Veeti Kauppinen <veeti.kauppinen@student.careeria.fi>
2025-04-03 12:43:01 +0300

    hellurei minä

---
+++ KalustoLuetteloSovellus/Controllers/KäyttäjätController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KalustoLuetteloSovellus.Models;
+
+namespace KalustoLuetteloSovellus.Controllers
+{
+    public class KäyttäjätController : Controller
+    {
+        private readonly KaluDbContext _context;
+
+        public KäyttäjätController(KaluDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Käyttäjät
+        public async Task<IActionResult> Index()
+        {
+            var kaluDbContext = _context.Käyttäjäs.Include(k => k.Rooli);
+            return View(await kaluDbContext.ToListAsync());
+        }
+
+        // GET: Käyttäjät/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var käyttäjä = await _context.Käyttäjäs
+                .Include(k => k.Rooli)
+                .FirstOrDefaultAsync(m => m.KäyttäjäId == id);
+            if (käyttäjä == null)
+            {
+                return NotFound();
+            }
+
+            return View(käyttäjä);
+        }
+
+        // GET: Käyttäjät/Create
+        public IActionResult Create()
+        {
+            ViewData["RooliId"] = new SelectList(_context.Roolis, "RooliId", "RooliId");
+            return View();
+        }
+
+        // POST: Käyttäjät/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("KäyttäjäId,Käyttäjätunnus,Salasana,RooliId")] Käyttäjä käyttäjä)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(käyttäjä);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RooliId"] = new SelectList(_context.Roolis, "RooliId", "RooliId", käyttäjä.RooliId);
+            return View(käyttäjä);
+        }
+
+        // GET: Käyttäjät/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var käyttäjä = await _context.Käyttäjäs.FindAsync(id);
+            if (käyttäjä == null)
+            {
+                return NotFound();
+            }
+            ViewData["RooliId"] = new SelectList(_context.Roolis, "RooliId", "RooliId", käyttäjä.RooliId);
+            return View(käyttäjä);
+        }
+
+        // POST: Käyttäjät/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("KäyttäjäId,Käyttäjätunnus,Salasana,RooliId")] Käyttäjä käyttäjä)
+        {
+            if (id != käyttäjä.KäyttäjäId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(käyttäjä);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KäyttäjäExists(käyttäjä.KäyttäjäId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RooliId"] = new SelectList(_context.Roolis, "RooliId", "RooliId", käyttäjä.RooliId);
+            return View(käyttäjä);
+        }
+
+        // GET: Käyttäjät/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var käyttäjä = await _context.Käyttäjäs
+                .Include(k => k.Rooli)
+                .FirstOrDefaultAsync(m => m.KäyttäjäId == id);
+            if (käyttäjä == null)
+            {
+                return NotFound();
+            }
+
+            return View(käyttäjä);
+        }
+
+        // POST: Käyttäjät/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var käyttäjä = await _context.Käyttäjäs.FindAsync(id);
+            if (käyttäjä != null)
+            {
+                _context.Käyttäjäs.Remove(käyttäjä);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KäyttäjäExists(int id)
+        {
+            return _context.Käyttäjäs.Any(e => e.KäyttäjäId == id);
+        }
+    }
+}
