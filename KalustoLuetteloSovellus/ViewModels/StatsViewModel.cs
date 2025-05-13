9ae04bc46de3088395aea59b42586152cd31304e
Niklas Granqvist <niklas.granqvist@student.careeria.fi>
2025-05-13 12:45:04 +0300

    Tein statistiikka piirakkakaavio partialin ja lisäsin etusivulle niitä

---
+++ KalustoLuetteloSovellus/ViewModels/StatsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace KalustoLuetteloSovellus.ViewModels
+{
+    public class StatsViewModel
+    {
+        public string Name { get; set; }
+        public int Total { get; set; }
+        public int Used { get; set; }
+    }
+}
